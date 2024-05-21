4d3c202e7f729fe2a4f2e3be6b4d1f70b16bbc57
user <user@host.docker.internal>
2024-05-21 23:46:42 +1200

    changes to main method so it can be accused from RulesDisplay

--- src/millionaire/RulesDisplay.java
+++ src/millionaire/RulesDisplay.java
@@ -1,15 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package millionaire;
 
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.awt.BorderLayout;
 
-/**
- *
- * @author user
- */
-public class RulesDisplay extends JPanel{
-    
+public class RulesDisplay extends JPanel {
+
+    private JTextArea rulesTextArea;
+
+    public RulesDisplay() {
+        setLayout(new BorderLayout());
+
+        rulesTextArea = new JTextArea();
+        rulesTextArea.setEditable(false);
+        add(rulesTextArea, BorderLayout.CENTER);
+
+        loadRules();
+    }
+
+    private void loadRules() {
+        StringBuilder rules = new StringBuilder();
+        String gameRulesFilePath = "rules.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(gameRulesFilePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                rules.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            rules.append("Rules could not be loaded.");
+        }
+
+        rulesTextArea.setText(rules.toString());
+    }
 }
